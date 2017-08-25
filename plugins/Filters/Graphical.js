1c4e000908896a0cda99421becc89eda75eace90
Nicholas Jillings <n.g.r.jillings@se14.qmul.ac.uk>
2017-08-25 10:46:05 +0100

    Add peaking filter and 10-band GEQ

---
+++ plugins/Filters/Graphical.js
@@ -0,0 +1,58 @@
+/*globals BasePlugin */
+/*
+    GraphicalEQ
+*/
+var GraphicalEQ = function (factory, owner) {
+    /* 
+        Each plugin is passed two arguments on construction:
+            1 - > Factory: The factory that built this plugin
+            2 - > Owner: The SubFactory that this plugin is registered too (if given)
+    */
+
+    // This attaches the base plugin items to the Object
+    BasePlugin.call(this, factory, owner);
+
+    /* USER MODIFIABLE BEGIN */
+    // Only modify between this line and the end of the object!
+
+    var input = this.context.createGain();
+    var output = this.context.createGain();
+
+    // Create a bunch of filter nodes
+    var filters = [],
+        N = 10;
+
+    for (var i = 0; i < N; i++) {
+        filters[i] = new PeakingFilter(factory, this);
+        var f = 20.0 * Math.pow(2, i);
+        filters[i].parameters.parameters.frequency.value = f;
+        filters[i].parameters.parameters.Q.value = 1.414;
+        var g = this.parameters.createNumberParameter(f.toFixed(2) + "Hz", 0, -12, +12);
+        g.bindToAudioParam(filters[i].parameters.parameters.gain);
+    }
+
+    for (var i = 0; i < N - 1; i++) {
+        filters[i].getOutputs()[0].connect(filters[i + 1].getInputs()[0]);
+    }
+    input.connect(filters[0].getInputs()[0]);
+    filters[N - 1].getOutputs()[0].connect(output);
+
+    this.addInput(input);
+    this.addOutput(output);
+
+    /* USER MODIFIABLE END */
+};
+
+// Also update the prototype function here!
+GraphicalEQ.prototype = Object.create(BasePlugin.prototype);
+GraphicalEQ.prototype.constructor = GraphicalEQ;
+GraphicalEQ.prototype.name = "GraphicalEQ";
+GraphicalEQ.prototype.version = "0.0.0";
+GraphicalEQ.prototype.uniqueID = "GREQ";
+GraphicalEQ.prototype.resources = [{
+    'url': "./peaking.js",
+    'type': "javascript",
+    'test': function () {
+        return typeof PeakingFilter === "function";
+    }
+}]
---
+++ plugins/Filters/peaking.js
@@ -0,0 +1,40 @@
+/*globals BasePlugin */
+/*
+    HPF
+    A 2nd order biquad high pass filter
+*/
+var PeakingFilter = function (factory, owner) {
+
+    // This attaches the base plugin items to the Object
+    BasePlugin.call(this, factory, owner);
+
+    /* USER MODIFIABLE BEGIN */
+    // Only modify between this line and the end of the object!
+
+    var inputNode = this.context.createGain(),
+        outputNode = this.context.createGain(),
+        filter = this.context.createBiquadFilter();
+
+    filter.type = "peaking";
+
+    inputNode.connect(filter);
+    filter.connect(outputNode);
+
+    var frequency = this.parameters.createNumberParameter("frequency", 1000, 10, 20000);
+    var gain = this.parameters.createNumberParameter("gain", 0, -24, +24);
+    var Q = this.parameters.createNumberParameter("Q", 1.414, 0.1, +12);
+
+    frequency.bindToAudioParam(filter.frequency);
+    gain.bindToAudioParam(filter.gain);
+    Q.bindToAudioParam(filter.Q);
+
+    this.addInput(inputNode);
+    this.addOutput(outputNode);
+};
+
+// Also update the prototype function here!
+PeakingFilter.prototype = Object.create(BasePlugin.prototype);
+PeakingFilter.prototype.constructor = PeakingFilter;
+PeakingFilter.prototype.name = "PeakingFilter";
+PeakingFilter.prototype.version = "1.0.0";
+PeakingFilter.prototype.uniqueID = "JSHP";
